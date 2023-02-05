4e2f60b37eb00485df3b38092243f095d1e61055
siddhant <suryawanshi.siddhant11@gmail.com>
2023-02-05 10:54:36 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+  db: {
+    /* don't expose password or any sensitive info, done only for demo */
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  listPerPage: 10,
+};
+module.exports = config;
